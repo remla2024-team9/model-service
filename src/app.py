9f8652b4dc6be9cc7f28708484affcfa3ea42f60
Samuel Ribaric <samuel.ribaric@live.se>
2024-05-13 16:48:39 +0200

    added functionality for google cloud

--- src/app.py
+++ src/app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, jsonify
 from remla2024_team9_lib_ml import tokenize_url  # Ensure the package is correctly named
-
+from tensorflow.keras.models import load_model
 app = Flask(__name__)
+
+#model_url = "https://www.dropbox.com/scl/fi/5giiub4m0aq0gvvnst7d9/best_model.keras?rlkey=nmmwvz9dz7fqjxfo5ll71lfy1&st=ttqjdxc5&dl=0"
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     if 'url' in data:
         # Tokenize the URL
+        model = load_model("models/best_model.keras") # Change to loading model from cloud
         tokenized_input = tokenize_url(data['url'])
+        output = model.predict(tokenized_input)
         # Dummy response for now, replace with actual model prediction logic
-        response = {"status": "received", "tokenized": tokenized_input}
+        response = {"status": "received", "tokenized": output}
     else:
         response = {"error": "URL not provided"}
     return jsonify(response)
