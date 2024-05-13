79d9082fa6cfc4f039237282fd979b08a61d16ce
Samuel Ribaric <samuel.ribaric@live.se>
2024-05-13 21:50:38 +0200

    added readme and finalizded details

--- src/app.py
+++ src/app.py
@@ -1,16 +1,23 @@
+"""
+This module sets up a Flask application that predicts data based on a given model and tokenizer, 
+and handles the downloading of data files from remote URLs.
+"""
+
+
+import pickle
 from flask import Flask, request, jsonify
 from keras.models import load_model
 from remla2024_team9_lib_ml import tokenize_url
-import pickle
 
 app = Flask(__name__)
-
 # Load the model and tokenizer
 model = load_model('models/model.keras')
-tokenizer = pickle.load(open('models/tokenizer.pkl', 'rb'))
+with open('models/tokenizer.pkl', 'rb') as tokenizer_file:
+    tokenizer = pickle.load(tokenizer_file)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    """Receives a URL via POST request and returns the prediction from the model."""
     data = request.get_json()
     if 'url' in data:
         tokenized_input = tokenize_url(tokenizer, data['url'])
@@ -19,6 +26,8 @@ def predict():
     else:
         response = {"error": "URL not provided"}
     return jsonify(response)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- src/downloader.py
+++ src/downloader.py
@@ -1,14 +1,15 @@
-url_model = "https://www.dropbox.com/scl/fi/uzfys196rsd0axqx3o3om/model.keras?"
-"rlkey=obet9rdk3x6kfkfnmghvft33h&st=bur0qvl7&dl=0"
-url_tokenizer ="https://www.dropbox.com/scl/fi/agkgy9nnwrs5kclnvz9ld/tokenizer.pkl"
-"?rlkey=dczufz4fv7d0dyg6hlz3mjqyg&st=wy13pwe7&dl=0"
-
 """
-This module handles the downloading of data files from remote URLs.
+This module handles the downloading of the model and tokenizer from remote URLs.
 """
 
 import os
 import requests
+
+# Constants for URL downloads
+URL_MODEL = "https://www.dropbox.com/scl/fi/uzfys196rsd0axqx3o3om/model.keras"
+"?rlkey=obet9rdk3x6kfkfnmghvft33h&st=bur0qvl7&dl=0"
+URL_TOKENIZER = "https://www.dropbox.com/scl/fi/agkgy9nnwrs5kclnvz9ld/tokenizer.pkl"
+"?rlkey=dczufz4fv7d0dyg6hlz3mjqyg&st=wy13pwe7&dl=0"
 
 def download_file(url, local_filename):
     """Downloads a file from a URL and saves it locally."""
@@ -24,14 +25,11 @@ def download_file(url, local_filename):
     else:
         print(f"File already exists: {local_filename}")
 
-
 def setup_models():
     """Set up the dataset by downloading and saving necessary files (if they don't already exist)"""
     files = {
-        "model.keras": "https://www.dropbox.com/scl/fi/uzfys196rsd0axqx3o3om/model.keras?"
-"rlkey=obet9rdk3x6kfkfnmghvft33h&st=bur0qvl7&dl=1",
-        "tokenizer.pkl": "https://www.dropbox.com/scl/fi/agkgy9nnwrs5kclnvz9ld/tokenizer.pkl"
-"?rlkey=dczufz4fv7d0dyg6hlz3mjqyg&st=wy13pwe7&dl=1"
+        "model.keras": URL_MODEL,
+        "tokenizer.pkl": URL_TOKENIZER
     }
     extract_path = "models/"
 
