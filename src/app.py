a8de01a6410c0f5dff158a9e28fe0327a37fca31
Samuel Ribaric <samuel.ribaric@live.se>
2024-05-14 22:24:55 +0200

    added CORS

--- src/app.py
+++ src/app.py
@@ -8,16 +8,17 @@ import pickle
 from flask import Flask, request, jsonify
 from keras.models import load_model
 from remla2024_team9_lib_ml import tokenize_url
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  
+
 # Load the model and tokenizer
-model = load_model('models/model.keras')
-with open('models/tokenizer.pkl', 'rb') as tokenizer_file:
-    tokenizer = pickle.load(tokenizer_file)
+model = load_model('/app/models/best_model.keras')
+tokenizer = pickle.load(open('/app/models/tokenizer.pkl', 'rb'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    """Receives a URL via POST request and returns the prediction from the model."""
     data = request.get_json()
     if 'url' in data:
         tokenized_input = tokenize_url(tokenizer, data['url'])
@@ -26,8 +27,6 @@ def predict():
     else:
         response = {"error": "URL not provided"}
     return jsonify(response)
-
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
